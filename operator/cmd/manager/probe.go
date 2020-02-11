6703d57b8ca7599127227e14c8899d84c500bc76
bob.huang <bob.huang@prophetstor.com>
2020-02-11 17:16:48 +0800

    Revise "Alameda-Operator" liveness/readiness probe to exit with 0.
    Signed-off-by: bob.huang <bob.huang@prophetstor.com>

--- operator/cmd/manager/probe.go
+++ operator/cmd/manager/probe.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/containers-ai/alameda/operator/pkg/probe"
 )
 
 func livenessProbe(cfg *probe.LivenessProbeConfig) {
 	// probe.LivenessProbe(cfg)
+	os.Exit(0)
 }
 
 func readinessProbe(cfg *probe.ReadinessProbeConfig) {
 	// probe.ReadinessProbe(cfg)
+	os.Exit(0)
 }
