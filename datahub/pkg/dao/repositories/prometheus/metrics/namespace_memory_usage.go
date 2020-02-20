d9f27d7de054774f9ef66a5bf0552745a04b9a0e
kit.lin <kit.lin@prophetstor.com>
2020-02-20 11:30:06 +0800

    [datahub] Make datahub compatible to read metrics data from prometheus on redhat.
    Signed-off-by: kit.lin <kit.lin@prophetstor.com>
    (cherry picked from commit 7e85b7ea2e9de92b0a63b370185b09b2fd9ff329)

--- datahub/pkg/dao/repositories/prometheus/metrics/namespace_memory_usage.go
+++ datahub/pkg/dao/repositories/prometheus/metrics/namespace_memory_usage.go
@@ -27,6 +27,12 @@ func (n NamespaceMemoryUsageRepository) ListNamespaceMemoryUsageBytesEntitiesByN
 	// Example of expression to query prometheus
 	// sum(container_memory_usage_bytes{pod_name!="",container_name!="",container_name!="POD",namespace=~"@n1"}) by (namespace)
 
+	var (
+		response  InternalPromth.Response
+		labelType = 0
+		err       error
+	)
+
 	prometheusClient, err := InternalPromth.NewClient(&n.PrometheusConfig)
 	if err != nil {
 		return nil, errors.Wrap(err, "new prometheus client failed")
@@ -37,21 +43,26 @@ func (n NamespaceMemoryUsageRepository) ListNamespaceMemoryUsageBytesEntitiesByN
 		option(&opt)
 	}
 
-	queryLabelsString := n.buildQueryLabelsStringByNamespaceNames(namespaceNames)
-	queryExpression := fmt.Sprintf(`%s{%s}`, ContainerMemoryUsageBytesMetricName, queryLabelsString)
-	stepTimeInSeconds := int64(opt.StepTime.Nanoseconds() / int64(time.Second))
-	queryExpression, err = InternalPromth.WrapQueryExpression(queryExpression, opt.AggregateOverTimeFunc, stepTimeInSeconds)
-	if err != nil {
-		return nil, errors.Wrap(err, "wrap query expression failed")
-	}
-	queryExpression = fmt.Sprintf(`sum(%s) by (%s)`, queryExpression, ContainerMemoryUsageBytesLabelNamespace)
+	for labelType = 0; labelType < AvailableLabelType; labelType++ {
+		queryLabelsString := n.buildQueryLabelsStringByNamespaceNames(namespaceNames, labelType)
+		queryExpression := fmt.Sprintf(`%s{%s}`, ContainerMemoryUsageBytesMetricName, queryLabelsString)
+		stepTimeInSeconds := int64(opt.StepTime.Nanoseconds() / int64(time.Second))
+		queryExpression, err = InternalPromth.WrapQueryExpression(queryExpression, opt.AggregateOverTimeFunc, stepTimeInSeconds)
+		if err != nil {
+			return nil, errors.Wrap(err, "wrap query expression failed")
+		}
+		queryExpression = fmt.Sprintf(`sum(%s) by (%s)`, queryExpression, ContainerMemoryUsageBytesLabelNamespace)
 
-	scope.Debugf("Query to prometheus: queryExpression: %+v, StartTime: %+v, EndTime: %+v, StepTime: %+v", queryExpression, opt.StartTime, opt.EndTime, opt.StepTime)
-	response, err := prometheusClient.QueryRange(ctx, queryExpression, opt.StartTime, opt.EndTime, opt.StepTime)
-	if err != nil {
-		return nil, errors.Wrap(err, "query prometheus failed")
-	} else if response.Status != InternalPromth.StatusSuccess {
-		return nil, errors.Errorf("query prometheus failed: receive error response from prometheus: %s", response.Error)
+		scope.Debugf("Query to prometheus: queryExpression: %+v, StartTime: %+v, EndTime: %+v, StepTime: %+v", queryExpression, opt.StartTime, opt.EndTime, opt.StepTime)
+		response, err = prometheusClient.QueryRange(ctx, queryExpression, opt.StartTime, opt.EndTime, opt.StepTime)
+		if err != nil {
+			return nil, errors.Wrap(err, "query prometheus failed")
+		} else if response.Status != InternalPromth.StatusSuccess {
+			return nil, errors.Errorf("query prometheus failed: receive error response from prometheus: %s", response.Error)
+		}
+		if len(response.Data.Result) != 0 {
+			break
+		}
 	}
 
 	entities, err := response.GetEntities()
@@ -79,18 +90,18 @@ func (n NamespaceMemoryUsageRepository) ListNamespaceMemoryUsageBytesEntitiesByN
 	return namespaceMemoryUsageBytesEntities, nil
 }
 
-func (n NamespaceMemoryUsageRepository) buildDefaultQueryLabelsString() string {
+func (n NamespaceMemoryUsageRepository) buildDefaultQueryLabelsString(labelType int) string {
 
 	var queryLabelsString = ""
-	queryLabelsString += fmt.Sprintf(`%s != "",`, ContainerMemoryUsageBytesLabelPodName)
-	queryLabelsString += fmt.Sprintf(`%s != "",`, ContainerMemoryUsageBytesLabelContainerName)
-	queryLabelsString += fmt.Sprintf(`%s != "POD"`, ContainerMemoryUsageBytesLabelContainerName)
+	queryLabelsString += fmt.Sprintf(`%s != "",`, AvailableContainerMemoryUsageBytesLabelPodName[labelType])
+	queryLabelsString += fmt.Sprintf(`%s != "",`, AvailableContainerMemoryUsageBytesLabelContainerName[labelType])
+	queryLabelsString += fmt.Sprintf(`%s != "POD"`, AvailableContainerMemoryUsageBytesLabelContainerName[labelType])
 	return queryLabelsString
 }
 
-func (n NamespaceMemoryUsageRepository) buildQueryLabelsStringByNamespaceNames(namespaceNames []string) string {
+func (n NamespaceMemoryUsageRepository) buildQueryLabelsStringByNamespaceNames(namespaceNames []string, labelType int) string {
 	var (
-		queryLabelsString = n.buildDefaultQueryLabelsString()
+		queryLabelsString = n.buildDefaultQueryLabelsString(labelType)
 	)
 
 	names := ""
