e56af5a0f574030d75f484070e7c5a2d0d1667ff
Frederic Huang <kuofu.huang@prophetstor.com>
2020-03-30 10:04:48 +0800

    send jobs directly if scaler check is not set
    Signed-off-by: Frederic Huang <kuofu.huang@prophetstor.com>

--- ai-dispatcher/pkg/dispatcher/predict_job_sender.go
+++ ai-dispatcher/pkg/dispatcher/predict_job_sender.go
@@ -355,19 +355,22 @@ func (dispatcher *predictJobSender) SendPredictJobs(rawData []*datahub_data.Rawd
 		for _, grp := range rawDatum.GetGroups() {
 			columns := grp.GetColumns()
 			for _, row := range grp.GetRows() {
-				scaledUnit, err := dispatcher.isUnitWatchedByScaler(unit, row.GetValues(), columns)
-				if err != nil || !scaledUnit {
-					jobID, _ := utils.GetJobID(unit, row.GetValues(), columns,
-						datahub_common.MetricType_METRICS_TYPE_UNDEFINED, granularity)
-					if err != nil {
-						scope.Errorf("[%s] Skip sending predict job due to get alamedascaler information failed: %s",
-							jobID, err.Error())
-					} else {
-						scope.Infof("[%s] Skip sending predict job due to the unit is not watched by any alamedascaler",
-							jobID)
+				if unit.UnitValueKeys != nil {
+					scaledUnit, err := dispatcher.isUnitWatchedByScaler(unit, row.GetValues(), columns)
+					if err != nil || !scaledUnit {
+						jobID, _ := utils.GetJobID(unit, row.GetValues(), columns,
+							datahub_common.MetricType_METRICS_TYPE_UNDEFINED, granularity)
+						if err != nil {
+							scope.Errorf("[%s] Skip sending predict job due to get alamedascaler information failed: %s",
+								jobID, err.Error())
+						} else {
+							scope.Infof("[%s] Skip sending predict job due to the unit is not watched by any alamedascaler",
+								jobID)
+						}
+						continue
 					}
-					continue
 				}
+
 				for _, metricType := range unit.MetricTypes {
 					jobID, err := utils.GetJobID(unit, row.GetValues(), columns,
 						metricType, granularity)
