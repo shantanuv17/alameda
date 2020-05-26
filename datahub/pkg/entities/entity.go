a67b5da16054620f7b664d965ac72561c1035a7e
kit.lin <kit.lin@prophetstor.com>
2020-05-26 16:11:32 +0800

    [datahub] Enhance entity library.
    Signed-off-by: kit.lin <kit.lin@prophetstor.com>

--- datahub/pkg/entities/entity.go
+++ datahub/pkg/entities/entity.go
@@ -2,23 +2,35 @@ package entities
 
 import (
 	"github.com/containers-ai/alameda/pkg/utils/log"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"reflect"
 	"strconv"
+	"time"
 )
 
 var (
 	scope = log.RegisterScope("entity", "datahub entity library", 0)
 )
 
+type Row struct {
+	Time    *timestamp.Timestamp
+	Columns []string
+	Values  []string
+}
+
 type Entity interface {
-	Populate(entity interface{}, columns, values []string)
-	Row(entity interface{}, columns []string) []string
+	Populate(entity interface{}, timestamp *timestamp.Timestamp, columns, values []string)
+	Row(entity interface{}, fields []string) *Row
 }
 
 type DatahubEntity struct {
+	Time *time.Time
 }
 
-func (p *DatahubEntity) Populate(entity interface{}, columns, values []string) {
+func (p *DatahubEntity) Populate(entity interface{}, timestamp *timestamp.Timestamp, columns, values []string) {
+	p.Time = p.timestamp(timestamp)
+
 	fields := reflect.TypeOf(entity).Elem()
 	for index, column := range columns {
 		for i := 1; i < fields.NumField(); i++ {
@@ -28,16 +40,7 @@ func (p *DatahubEntity) Populate(entity interface{}, columns, values []string) {
 				case reflect.Bool:
 					valueBool, _ := strconv.ParseBool(values[index])
 					fieldValue.SetBool(valueBool)
-				case reflect.Int:
-					valueInt, _ := strconv.ParseInt(values[index], 10, 32)
-					fieldValue.SetInt(valueInt)
-				case reflect.Int8:
-					valueInt, _ := strconv.ParseInt(values[index], 10, 32)
-					fieldValue.SetInt(valueInt)
-				case reflect.Int16:
-					valueInt, _ := strconv.ParseInt(values[index], 10, 32)
-					fieldValue.SetInt(valueInt)
-				case reflect.Int32:
+				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32:
 					valueInt, _ := strconv.ParseInt(values[index], 10, 32)
 					fieldValue.SetInt(valueInt)
 				case reflect.Int64:
@@ -60,38 +63,59 @@ func (p *DatahubEntity) Populate(entity interface{}, columns, values []string) {
 	}
 }
 
-func (p *DatahubEntity) Row(entity interface{}, columns []string) []string {
-	row := make([]string, 0)
-	fields := reflect.TypeOf(entity).Elem()
-	for _, column := range columns {
-		for i := 1; i < fields.NumField(); i++ {
-			if column == fields.Field(i).Tag.Get("json") {
+func (p *DatahubEntity) Row(entity interface{}, fields []string) *Row {
+	row := Row{Time: p.timestampProto()}
+
+	// If fields is empty which means to iterate through all the fields of the entity
+	values := reflect.TypeOf(entity).Elem()
+	fieldNames := fields
+	if len(fieldNames) == 0 {
+		for i := 1; i < values.NumField(); i++ {
+			fieldNames = append(fieldNames, values.Field(i).Name)
+		}
+	}
+
+	for _, name := range fieldNames {
+		for i := 1; i < values.NumField(); i++ {
+			if name == values.Field(i).Name {
 				fieldValue := reflect.ValueOf(entity).Elem().Field(i)
 				switch fieldValue.Kind() {
 				case reflect.Bool:
-					row = append(row, strconv.FormatBool(fieldValue.Bool()))
-				case reflect.Int:
-					row = append(row, strconv.FormatInt(fieldValue.Int(), 10))
-				case reflect.Int8:
-					row = append(row, strconv.FormatInt(fieldValue.Int(), 10))
-				case reflect.Int16:
-					row = append(row, strconv.FormatInt(fieldValue.Int(), 10))
-				case reflect.Int32:
-					row = append(row, strconv.FormatInt(fieldValue.Int(), 10))
-				case reflect.Int64:
-					row = append(row, strconv.FormatInt(fieldValue.Int(), 10))
+					row.Columns = append(row.Columns, values.Field(i).Tag.Get("json"))
+					row.Values = append(row.Values, strconv.FormatBool(fieldValue.Bool()))
+				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+					row.Columns = append(row.Columns, values.Field(i).Tag.Get("json"))
+					row.Values = append(row.Values, strconv.FormatInt(fieldValue.Int(), 10))
 				case reflect.Float32:
-					row = append(row, strconv.FormatFloat(fieldValue.Float(), 'f', -1, 32))
+					row.Columns = append(row.Columns, values.Field(i).Tag.Get("json"))
+					row.Values = append(row.Values, strconv.FormatFloat(fieldValue.Float(), 'f', -1, 32))
 				case reflect.Float64:
-					row = append(row, strconv.FormatFloat(fieldValue.Float(), 'f', -1, 64))
+					row.Columns = append(row.Columns, values.Field(i).Tag.Get("json"))
+					row.Values = append(row.Values, strconv.FormatFloat(fieldValue.Float(), 'f', -1, 64))
 				case reflect.String:
-					row = append(row, fieldValue.String())
+					row.Columns = append(row.Columns, values.Field(i).Tag.Get("json"))
+					row.Values = append(row.Values, fieldValue.String())
 				default:
 					scope.Errorf("field type(%s) not supported", fieldValue.Kind().String())
 				}
-				break
 			}
 		}
 	}
-	return row
+
+	return &row
+}
+
+func (p *DatahubEntity) timestamp(timestamp *timestamp.Timestamp) *time.Time {
+	ts, _ := ptypes.Timestamp(timestamp)
+	return &ts
+}
+
+func (p *DatahubEntity) timestampProto() *timestamp.Timestamp {
+	ts := &timestamp.Timestamp{}
+	if p.Time == nil {
+		ts, _ = ptypes.TimestampProto(time.Unix(0, 0).UTC())
+	} else {
+		ts, _ = ptypes.TimestampProto(*p.Time)
+	}
+	return ts
 }
--- datahub/pkg/entities/execution.go
+++ datahub/pkg/entities/execution.go
@@ -1,14 +1,14 @@
 package entities
 
-type ExecutionClusterAutoscalerMachineset struct {
-	DatahubEntity
-	Name             string `json:"name"              required:"true" column:"tag"   type:"DATATYPE_STRING"`
-	Namespace        string `json:"namespace"         required:"true" column:"tag"   type:"DATATYPE_STRING"`
-	ClusterName      string `json:"cluster_name"      required:"true" column:"tag"   type:"DATATYPE_STRING"`
-	MachinegroupName string `json:"machinegroup_name" required:"true" column:"tag"   type:"DATATYPE_STRING"`
-	ExecutionTime    string `json:"execution_time"    required:"true" column:"field" type:"DATATYPE_STRING"`
-	ReplicasFrom     int32  `json:"replicas_from"     required:"true" column:"field" type:"DATATYPE_INT32"`
-	ReplicasTo       int32  `json:"replicas_to"       required:"true" column:"field" type:"DATATYPE_INT32"`
-	DeltaUpTime      int64  `json:"delta_up_time​"     required:"true" column:"field" type:"DATATYPE_INT64"`
-	DeltaDownTime    int64  `json:"delta_down_time"   required:"true" column:"field" type:"DATATYPE_INT64"`
+type ExecutionMachineset struct {
+	DatahubEntity    `scope:"SCOPE_EXECUTION"  category:"cluster_autoscaler"   type:"machineset"`
+	Name             string `json:"name"              required:"true"  column:"tag"   type:"DATATYPE_STRING"`
+	Namespace        string `json:"namespace"         required:"true"  column:"tag"   type:"DATATYPE_STRING"`
+	ClusterName      string `json:"cluster_name"      required:"true"  column:"tag"   type:"DATATYPE_STRING"`
+	MachinegroupName string `json:"machinegroup_name" required:"true"  column:"tag"   type:"DATATYPE_STRING"`
+	ExecutionTime    string `json:"execution_time"    required:"false" column:"field" type:"DATATYPE_STRING"`
+	ReplicasFrom     int32  `json:"replicas_from"     required:"false" column:"field" type:"DATATYPE_INT32"`
+	ReplicasTo       int32  `json:"replicas_to"       required:"false" column:"field" type:"DATATYPE_INT32"`
+	DeltaUpTime      int64  `json:"delta_up_time​"     required:"false" column:"field" type:"DATATYPE_INT64"`
+	DeltaDownTime    int64  `json:"delta_down_time"   required:"false" column:"field" type:"DATATYPE_INT64"`
 }
