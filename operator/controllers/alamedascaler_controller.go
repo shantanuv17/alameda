e2108176b352272e67b95a62ba147307ab1bff69
Frederic Huang <kuofu.huang@prophetstor.com>
2020-07-03 14:57:01 +0800

    fix kafka/nginx scaler is created but application record is not added immediately
    Signed-off-by: Frederic Huang <kuofu.huang@prophetstor.com>

--- operator/controllers/alamedascaler_controller.go
+++ operator/controllers/alamedascaler_controller.go
@@ -164,14 +164,12 @@ func (r *AlamedaScalerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 	alamedaScaler := autoscalingv1alpha1.AlamedaScaler{}
 	instance.DeepCopyInto(&alamedaScaler)
 
-	ok, err := r.isAlamedaScalerNeedToBeReconciled(context.TODO(), alamedaScaler)
-	if err != nil {
-		scope.Warnf("Check if AlamedaScaler(%s/%s) need to be reconciled failed, retry after %f seconds: %+v",
-			req.Namespace, req.Name, requeueAfter.Seconds(), err)
-		return ctrl.Result{Requeue: true, RequeueAfter: requeueAfter}, nil
-	} else if !ok {
-		scope.Infof("AlamedaScale(%s/%s) type(%s), skip reconciling.",
-			req.Namespace, req.Name, alamedaScaler.GetType())
+	if !r.isAlamedaScalerNeedToBeReconciled(context.TODO(), alamedaScaler) {
+		scope.Infof("AlamedaScale(%s/%s) type(%s), skip reconciling.", req.Namespace, req.Name, alamedaScaler.GetType())
+		if err := r.syncDatahubApplicationsByAlamedaScaler(ctx, alamedaScaler); err != nil {
+			scope.Errorf("sync alamedascaler(%s/%s) with datahub failed.", req.Namespace, req.Name)
+			return ctrl.Result{Requeue: true, RequeueAfter: requeueAfter}, nil
+		}
 		return ctrl.Result{Requeue: false}, nil
 	}
 
@@ -257,12 +255,9 @@ func (r AlamedaScalerReconciler) setDefaultAlamedaScaler(
 }
 
 func (r AlamedaScalerReconciler) isAlamedaScalerNeedToBeReconciled(
-	ctx context.Context, alamedaScaler autoscalingv1alpha1.AlamedaScaler) (bool, error) {
-	if alamedaScaler.GetType() == autoscalingv1alpha1.AlamedaScalerTypeNotDefine ||
-		alamedaScaler.GetType() == autoscalingv1alpha1.AlamedaScalerTypeDefault {
-		return true, nil
-	}
-	return false, nil
+	ctx context.Context, alamedaScaler autoscalingv1alpha1.AlamedaScaler) bool {
+	return alamedaScaler.GetType() == autoscalingv1alpha1.AlamedaScalerTypeNotDefine ||
+		alamedaScaler.GetType() == autoscalingv1alpha1.AlamedaScalerTypeDefault
 }
 
 func (r AlamedaScalerReconciler) isWorkloadControllerCanBeMonitoredByAlamedaScaler(
