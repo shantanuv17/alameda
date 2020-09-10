a7850374a3968a1594836dc5cb8fa73ab55497d6
ytliu <yungtsun.liu@prophetstor.com>
2020-09-10 16:58:13 +0800

    [datahub][keycode] keycode check cpu core limit by add all of nodes cpu cores

--- datahub/pkg/account-mgt/keycodes/utils.go
+++ datahub/pkg/account-mgt/keycodes/utils.go
@@ -24,30 +24,20 @@ func getClusterNodesFromDB(dao types.NodeDAO, clusterName string) ([]*types.Node
 func GetAlamedaClusterCPUs(influxCfg *InfluxDB.Config) (int, error) {
 	numCPU := 0
 	clusterName := ""
-	dbCluster := influxdb.NewClusterWithConfig(*influxCfg)
+
 	dbNode := influxdb.NewNodeWithConfig(*influxCfg)
 
-	clusters, err := getClustersFromDB(dbCluster)
+	nodes, err := getClusterNodesFromDB(dbNode, clusterName)
 	if err != nil {
-		scope.Errorf("Failed to get cluster info: %s", err.Error())
+		scope.Errorf("Failed to get cluster [%s] node info: %s", clusterName, err.Error())
 		return numCPU, err
-	}
-	if clusters != nil && len(clusters) > 0 {
-		clusterName = clusters[0].ObjectMeta.Name
-		scope.Infof("check cluster [%s] CPU info", clusterName)
-	}
-	if clusterName != "" {
-		nodes, err := getClusterNodesFromDB(dbNode, clusterName)
-		if err != nil {
-			scope.Errorf("Failed to get cluster [%s] node info: %s", clusterName, err.Error())
-			return numCPU, err
-		} else {
-			scope.Infof("Number of cluster node: %d", len(nodes))
-			for _, node := range nodes {
-				if node != nil {
-					scope.Infof("  node: %s, CPU Cores: %d", node.ObjectMeta.Name, node.Capacity.CpuCores)
-					numCPU += int(node.Capacity.CpuCores)
-				}
+	} else {
+		scope.Infof("Number of node: %d", len(nodes))
+		for _, node := range nodes {
+			if node != nil {
+				scope.Infof("  node: %s (cluster: %s), CPU Cores: %d", node.ObjectMeta.Name,
+					node.ObjectMeta.ClusterName, node.Capacity.CpuCores)
+				numCPU += int(node.Capacity.CpuCores)
 			}
 		}
 	}
